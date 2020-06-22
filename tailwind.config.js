338aeea396c7668f4d6d01afcdd663f19ba31fbf
mostfaswaisi93 <mostfaswaisi@gmail.com>
2020-06-22 13:13:23 +0300

    npm run serve

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
     purge: [],
     theme: {
-        extend: {},
+        extend: {
+            spacing: {
+                '80': '20rem'
+            },
+            rotate: {
+                '2': '2deg',
+                '4': '4deg',
+                '6': '6deg',
+                '8': '8deg',
+                '10': '10deg',
+                '12': '12deg',
+            }
+        },
     },
     variants: {},
     plugins: [],
